a4a9b74540fa88c591b54b9867900e6b0f81f9c7
Anne Chinn <achinn@nwlink.com>
2021-11-03 12:00:22 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const gameboard = document.getElementById("gameboard");
+const boxes = Array.from(document.getElementsByClassName("box"));
+const restartBtn = document.getElementById("restartBtn");
+const playText = document.getElementById("playText");
+
